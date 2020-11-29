899372b82ffd43eb613ee2d48a8b340935d5a89a
Michał Domino <michal.domino@yahoo.com>
2020-11-29 14:12:14 +0100

    Add email field, add password validation

--- apps/authentication/serializers/register_user_serializer.py
+++ apps/authentication/serializers/register_user_serializer.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 UserModel = get_user_model()
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, validators=[validate_password])
 
     def create(self, validated_data):
-        user = UserModel.objects.create(
-            username=validated_data['username']
-        )
+        user = UserModel.objects.create(username=validated_data['username'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
 
@@ -18,4 +18,4 @@ class RegisterUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserModel
-        fields = ("id", "username", "password",)
+        fields = ('id', 'username', 'email', 'password',)
